ed44b8c45851973fc936791746f4bc47bfc10058
Jana Lasser <jana.lasser@uni-graz.at>
2025-05-17 06:54:37 +0200

    split into three different calls for three different data sets

--- code/label_glove840B_DDR.sh
+++ code/label_glove840B_DDR.sh
@@ -3,13 +3,35 @@
 source_path="./"
 model_name_or_path="../data/utilities/sentence-transformers/glove-model-reduced-stopwords"
 
-# compute belief-speaking and fact-speaking similarities
+# compute belief-speaking and fact-speaking similarities of the first pilot data set
 python ${source_path}/compute_sbert_avg_lexicon.py --model_name_or_path ${model_name_or_path}\
-	--input_file "../data/combined_midterm_candidate_timelines_2022-01-01_to_2023-05-01_clean.csv.gzip"\
-	--output_file "../data/combined_midterm_candidate_timelines_2022-01-01_to_2023-05-01_honesty_component_scores.csv.gzip"\
+	--input_file "../data/pilot_data1_raw.csv.gzip"\
+	--output_file "../data/pilot_data1_honesty_component_scores.csv.gzip"\
 	--fact_lexicon "../data/utilities/fact_speaking_p=0.05_swapped_wn_def_example.csv"\
 	--belief_lexicon "../data/utilities/belief_speaking_p=0.05_swapped_wn_def_example.csv"\
 	--avg_dict --average_of_similarity\
-   --compression_type "gzip"\
+    --compression_type "gzip"\
 	--corpus "Twitter"\
+   #--smoke_test
+
+# compute belief-speaking and fact-speaking similarities of the second pilot data set
+python ${source_path}/compute_sbert_avg_lexicon.py --model_name_or_path ${model_name_or_path}\
+	--input_file "../data/pilot_data2_raw.csv.gzip"\
+	--output_file "../data/pilot_data2_honesty_component_scores.csv.gzip"\
+	--fact_lexicon "../data/utilities/fact_speaking_p=0.05_swapped_wn_def_example.csv"\
+	--belief_lexicon "../data/utilities/belief_speaking_p=0.05_swapped_wn_def_example.csv"\
+	--avg_dict --average_of_similarity\
+    --compression_type "gzip"\
+	--corpus "Twitter"\
+   #--smoke_test
+
+# compute belief-speaking and fact-speaking similarities of the study data set
+#python ${source_path}/compute_sbert_avg_lexicon.py --model_name_or_path ${model_name_or_path}\
+	#--input_file "../data/study_data_raw.csv.gzip"\
+	#--output_file "../data/study_data_honesty_component_scores.csv.gzip"\
+	#--fact_lexicon "../data/utilities/fact_speaking_p=0.05_swapped_wn_def_example.csv"\
+	#--belief_lexicon "../data/utilities/belief_speaking_p=0.05_swapped_wn_def_example.csv"\
+	#--avg_dict --average_of_similarity\
+    #--compression_type "gzip"\
+	#--corpus "Twitter"\
    #--smoke_test
